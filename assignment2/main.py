92bf514d3413d73782b31d622b229416137bd06a
vespenegas <andyvu95@gmail.com>
2023-10-04 20:33:36 +0300

    fin

--- assignment2/main.py
+++ assignment2/main.py
@@ -2,55 +2,144 @@
 Simple indexer and search engine built on an inverted-index and the BM25 ranking algorithm.
 """
 import os
-from collections import defaultdict, Counter
+from collections import defaultdict
 import pickle
 import math
-import operator
-
 from tqdm import tqdm
 from nltk import pos_tag
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from datasets import load_dataset
+import code
+# commented out the import and download of the stopwords and word net as these only needed to be run once
+# import nltk
+# nltk.download('wordnet')
+# nltk.download('stopwords')
 
-
+# indexer class
 class Indexer:
     dbfile = "./ir.idx"  # This is the index file you will create and manager for indexing
 
     def __init__(self):
-        # TODO. You will need to create an appropriate data structure for the following elements
-        # self.tok2idx = None                       # map (token to id)
-        # self.idx2tok = None                       # map (id to token)
-        # self.postings_lists = None                # postings for each word
-        # self.docs = []                            # encoded document list
-        # self.raw_ds = None                        # raw documents for search results
-        # self.corpus_stats = { 'avgdl': 0 }        # any corpus-level statistics
-        # self.stopwords = stopwords.words('english')
+        # data structures to store and manage elements from the search engine
 
-        if os.path.exists(dbfile):
-            # TODO. If there exists a saved corpus index file, load data from it.
-            # (You may use Python Pickle to save and load a python object.)
+        self.tok2idx = {}                       # dictionary for token to id mapping
+        self.idx2tok = {}                       # dictionary for id to token mapping
+        self.postings_lists = {}                # term to postings list mapping
+        self.docs = []                          # list to store document information
+        self.raw_ds = []                        # list to store raw documents from search result
+        self.corpus_stats = { 'avgdl': 0 }      # dictionary for corpus level statistics
+        self.stopwords = stopwords.words('english') # list of stopwords from NLTK library of stopwords
+
+        if os.path.exists(self.dbfile):
+            # if the index file exist, load the data from it
+            self.load_index_data()
+            
             pass
         else:
-            # TODO. Load CNN/DailyMail dataset, preprocess and create postings lists.
+            # if the index file does not exist, load and preprocess the dataset
+
             ds = load_dataset("cnn_dailymail", '3.0.0', split="test")
             self.raw_ds = ds['article']
             self.clean_text(self.raw_ds)
             self.create_postings_lists()
 
+    def load_index_data(self):
+        # load the data from the index file using pickle
+        # open the index file in binary read mode 'rb'
+        # use pickle.load to deserialize the data 
+        with open (self.dbfile, 'rb') as f:
+            index_data = pickle.load(f)
+
+        # populate the data structures with the loaded data
+        self.tok2idx = index_data['tok2idx']
+        self.idx2tok = index_data['idx2tok']
+        self.postings_lists = index_data['postings_lists']
+        self.docs = index_data['docs']
+        self.raw_ds = index_data['raw_ds']
+        self.corpus_stats = index_data['corpus_stats']
+        self.stopwords = index_data['stopwords']
+
+    def save_index_data(self):
+        # create a dictionary to hold the index data
+        index_data = {
+            'tok2idx': self.tok2idx,
+            'idx2tok': self.idx2tok,
+            'postings_lists': self.postings_lists,
+            'docs': self.docs,
+            'raw_ds': self.raw_ds,
+            'corpus_stats': self.corpus_stats,
+            'stopwords': self.stopwords
+        }
+
+        # serialize and save index data to the index file using pickle
+        with open(self.dbfile, 'wb') as f:
+            pickle.dump(index_data, f)
+            
+
     def clean_text(self, lst_text, query=False):
-        # TODO. this function can be used for either one (or both) of indexing and querying process
-        # TODO. run simple whitespace-based tokenizer (e.g., RegexpTokenizer)
-        # TODO. run lemmatizer (e.g., WordNetLemmatizer)
-        # TODO. for indexing, read documents one by one and process
-        raise NotImplementedError
+        # Initialize tokenizer and lemmatizer
+        tokenizer = RegexpTokenizer(r'\w+')
+        lemmatizer = WordNetLemmatizer()
+
+        # Initialize a list for the cleaned text
+        cleaned_text = [] 
+
+        for text in lst_text:
+            tokens = tokenizer.tokenize(text.lower())  # Tokenize and lowercase
+
+            if not query:
+                # Remove stopwords for indexing (not for query)
+                tokens = [token for token in tokens if token not in self.stopwords]
+
+            lemmatized_tokens = []
+            for token in tokens:
+                lemmatized_tokens.append(lemmatizer.lemmatize(token))  # Lemmatize
+
+            # Join the tokens back into a single string
+            cleaned_text.append(" ".join(lemmatized_tokens))
+
+        if not query:
+            self.docs = cleaned_text  # Assign the cleaned text to self.docs for indexing
+
+        return cleaned_text
 
     def create_postings_lists(self):
-        # TODO. This creates postings lists of your corpus
-        # TODO. While indexing compute avgdl and document frequencies of your vocabulary
-        # TODO. Save it, so you don't have to do this again in the next runs.
-        raise NotImplementedError
+        # Initialize data structures for postings and document statistics
+        self.postings_lists = defaultdict(list)
+        doc_lengths = []
+        df = defaultdict(int)
+
+        for doc_id, doc_text in enumerate(self.docs):
+            # Tokenize the document
+            tokens = doc_text.split()
+
+            # Update document length
+            doc_length = len(tokens)
+            doc_lengths.append(doc_length)
+
+            # Compute document frequencies (df)
+            unique_tokens = set(tokens)
+            for token in unique_tokens:  # Use unique tokens to count each term only once per document
+                df[token] += 1
+
+            # Create postings list
+            for token in unique_tokens:  # Use unique tokens to create postings list
+                self.postings_lists[token].append(doc_id)
+
+        # Compute the average document length (avgdl)
+        total_doc_length = sum(doc_lengths)
+        num_docs = len(self.docs)
+        avgdl = total_doc_length / num_docs
+        self.corpus_stats['avgdl'] = avgdl
+
+        # Create an index data dictionary to store postings lists, document frequencies, and avgdl
+        index_data = {
+            'postings_lists': dict(self.postings_lists),
+            'document_frequencies': dict(df),
+            'avgdl': avgdl
+        }
 
 
 class SearchAgent:
@@ -58,30 +147,69 @@ class SearchAgent:
     b = 0.75                # BM25 parameter b for document length normalization
 
     def __init__(self, indexer):
-        # TODO. set necessary parameters
-        self.i = indexer
+        
+        self.indexer = indexer
 
     def query(self, q_str):
-        # TODO. This is take a query string from a user, run the same clean_text process,
-        # TODO. Calculate BM25 scores
-        # TODO. Sort  the results by the scores in decsending order
-        # TODO. Display the result
+        # process the query using the same clean_text process
+        cleaned_query = self.indexer.clean_text([q_str], query=True)[0]
 
+        # Calculate BM25 scores for documents
+        results = self.calculate_bm25_scores(cleaned_query)
+
+        # Sort the results by scores in descending order
+        sorted_results = sorted(results.items(), key=lambda x: x[1], reverse=True)
+
+        # display results
+        self.display_results(sorted_results)
+
+    def calculate_bm25_scores(self, cleaned_query):
+        # Create a dictionary to store document scores
         results = {}
-        if len(results) == 0:
-            return None
-        else:
-            self.display_results(results)
 
+        # Calculate IDF (inverse document frequency) for each query term
+        query_terms = cleaned_query.split()
+        idf_values = {}
+        for term in query_terms:
+            df_term = len(self.indexer.postings_lists.get(term, []))
+            idf_term = math.log((len(self.indexer.docs) - df_term + 0.5) / (df_term + 0.5) + 1.0)
+            idf_values[term] = idf_term
+
+        # Iterate through documents and calculate scores
+        for doc_id, doc_text in enumerate(self.indexer.docs):
+            doc_score = 0.0
+
+            # Tokenize the document
+            doc_terms = doc_text.split()
+
+            # Calculate document length (|D|)
+            doc_length = len(doc_terms)
+
+            # Calculate average document length (avgdl) from corpus stats
+            avgdl = self.indexer.corpus_stats['avgdl']
+
+            for term in query_terms:
+                # Calculate TF (term frequency) for the term in the document
+                tf_term = doc_terms.count(term)
+
+                # Calculate BM25 score for the term in the document
+                score_term = (idf_values[term] * tf_term * (self.k1 + 1)) / (
+                    tf_term + self.k1 * ((1 - self.b) + self.b * (doc_length / avgdl))
+                )
+
+                doc_score += score_term
+
+            results[doc_id] = doc_score
+
+        return results
 
     def display_results(self, results):
-        # Decode
-        # TODO, the following is an example code, you can change however you would like.
+
         for docid, score in results[:5]:  # print top 5 results
             print(f'\nDocID: {docid}')
             print(f'Score: {score}')
             print('Article:')
-            print(self.i.raw_ds[docid])
+            print(self.indexer.raw_ds[docid])
 
 
 
